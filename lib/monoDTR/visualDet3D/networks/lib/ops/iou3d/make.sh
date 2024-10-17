703aa9eb112f2702e2df034451a0b3ee4a302a86
Akos0628 <75949273+Akos0628@users.noreply.github.com>
2024-10-17 22:27:58 +0200

    added combined detection

---
+++ lib/monoDTR/visualDet3D/networks/lib/ops/iou3d/make.sh
@@ -0,0 +1,14 @@
+export CUDA_HOME=/usr/local/cuda
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda
+export CUDA_TOOLKIT_ROOT_DIR=$CUDA_HOME
+export LD_LIBRARY_PATH="$CUDA_HOME/extras/CUPTI/lib64:$LD_LIBRARY_PATH"
+export LIBRARY_PATH=$CUDA_HOME/lib64:$LIBRARY_PATH
+export LD_LIBRARY_PATH=$CUDA_HOME/lib64:$LD_LIBRARY_PATH
+export CFLAGS="-fopenmp -I$CUDA_HOME/include $CFLAGS"
+export TORCH_CUDA_ARCH_LIST=$(python3 -c "import torch;a=torch.cuda.get_device_capability(0);print(str(a[0])+'.'+str(a[1]))")
+
+export CC=gcc-11
+
+python3 setup.py build_ext --inplace
+
+rm -r build
